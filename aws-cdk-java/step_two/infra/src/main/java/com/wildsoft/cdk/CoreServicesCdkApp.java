a4b107259dcfdf29cf27a56fa290423947285464
Herman Lintvelt <herman.lintvelt@gmail.com>
2022-11-01 18:55:41 +0200

    trying something with pipelines

--- aws-cdk-java/step_two/infra/src/main/java/com/wildsoft/cdk/CoreServicesCdkApp.java
+++ aws-cdk-java/step_two/infra/src/main/java/com/wildsoft/cdk/CoreServicesCdkApp.java
@@ -12,21 +12,26 @@ public final class CoreServicesCdkApp {
 
     public static void main(final String[] args) {
         App app = new App();
+        //TODO: uncomment this and commnet in the if-else below to get it deployed first time
+        final CoreServicesCICDStack pipelineStack = new CoreServicesCICDStack(app, "Staging-CICD-Stack");
+        final String permissionBoundaryArn = Fn.importValue("CICDPipelinePermissionsBoundaryArn");
+        IManagedPolicy policy = ManagedPolicy.fromManagedPolicyArn(pipelineStack, "CICDPipelinePermissionsBoundary", permissionBoundaryArn);
+        PermissionsBoundary.of(pipelineStack).apply(policy);
 
-        if (System.getenv("CODEBUILD_BUILD_ID") != null) {
-            LOG.info("Starting CDK App - CICD Stack");
-            //running on CI/CD trigger
-            final Stack pipelineStack = new CoreServicesCICDStack(app, "CoreServicesCICDStack");
-
-            final String permissionBoundaryArn = Fn.importValue("CICDPipelinePermissionsBoundaryArn");
-            IManagedPolicy policy = ManagedPolicy.fromManagedPolicyArn(pipelineStack, "CICDPipelinePermissionsBoundary", permissionBoundaryArn);
-            PermissionsBoundary.of(pipelineStack).apply(policy);
-        } else {
-            LOG.info("Starting CDK App - DEV Stack");
-            //running manual from DEV
-            //comment out this option and if statement and deploy CICD directly if need to renew pipeline manually
-            new CoreServicesStack(app, "CoreServicesDEVStack", "development");
-        }
+//        if (System.getenv("CODEBUILD_BUILD_ID") != null) {
+//            LOG.info("Starting CDK App - CICD Stack");
+//            //running on CI/CD trigger
+//            new CoreServicesCICDStack(app, "Staging-CICD-Stack");
+//
+//            final String permissionBoundaryArn = Fn.importValue("CICDPipelinePermissionsBoundaryArn");
+//            IManagedPolicy policy = ManagedPolicy.fromManagedPolicyArn(pipelineStack, "CICDPipelinePermissionsBoundary", permissionBoundaryArn);
+//            PermissionsBoundary.of(pipelineStack).apply(policy);
+//        } else {
+//            LOG.info("Starting CDK App - DEV Stack");
+//            //running manual from DEV
+//            //comment out this option and if statement and deploy CICD directly if need to renew pipeline manually
+//            new CoreServicesStack(app, "CoreServicesDEVStack", "development");
+//        }
 
         //always keep this here, for direct deploy or via pipeline
         app.synth();
